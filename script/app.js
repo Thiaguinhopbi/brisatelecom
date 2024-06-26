30f3cea8eeff5054738eff36ecd643df1f5c67dc
Paulo Victor <paulpbi16@gmail.com>
2024-06-26 20:20:33 -0300

    feat: adicionada animacao ao dar scroll na pagina

--- script/app.js
+++ script/app.js
@@ -8,7 +8,35 @@ function adicionarDinamicamenteAno() {
     } else {
         return;
     }
-    
 }
 
-adicionarDinamicamenteAno()
+adicionarDinamicamenteAno();
+
+// cria o efeito de animação ao scroll
+function ativarScroll() {
+  const elementoScroll = document.querySelectorAll(".js .js-scroll");
+  if (elementoScroll.length) {
+    const alturaTela = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      elementoScroll.forEach((section) => {
+        const alturaElemento = section.getBoundingClientRect().top;
+        const metadeTela = alturaElemento - alturaTela < 0;
+
+        if (metadeTela) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+    animaScroll();
+  }
+
+  //mantem a primeira section ativa
+  elementoScroll[0].classList.add("ativo");
+}
+
+window.addEventListener("scroll", ativarScroll);
+
+ativarScroll();
