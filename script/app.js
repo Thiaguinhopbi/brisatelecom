024343f14fd1f5fcf643624b92a497f2fee67596
Paulo Victor <paulpbi16@gmail.com>
2024-06-26 20:21:36 -0300

    formatacao do codigo e foi excluido variaveis nao utilizadas

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 // adiona o ano dinamicamente no footer
 function adicionarDinamicamenteAno() {
-    const data = new Date();
+  const data = new Date();
     if(data) {
         const elementoFooter = document.querySelector(".footer-data");
         const ano = data.getFullYear;
