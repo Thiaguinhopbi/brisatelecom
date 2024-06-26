c814d5b4852b1484e33750cb89fe5d288a21c789
Paulo Victor <paulpbi16@gmail.com>
2024-06-26 20:22:04 -0300

    formatacao do codigo e foi excluido variaveis nao utilizadas

--- script/app.js
+++ script/app.js
@@ -1,13 +1,12 @@
 // adiona o ano dinamicamente no footer
 function adicionarDinamicamenteAno() {
   const data = new Date();
-    if(data) {
-        const elementoFooter = document.querySelector(".footer-data");
-        const ano = data.getFullYear;
-        elementoFooter.innerHTML = ` &copy; Brisa Telecom &middot; Todos os direitos reservados ${data.getFullYear()}.`;
-    } else {
-        return;
-    }
+  if (data) {
+    const elementoFooter = document.querySelector(".footer-data");
+    elementoFooter.innerHTML = ` &copy; Brisa Telecom &middot; Todos os direitos reservados ${data.getFullYear()}.`;
+  } else {
+    return;
+  }
 }
 
 adicionarDinamicamenteAno();
